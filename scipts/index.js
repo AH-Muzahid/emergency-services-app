7e68261217ea1ca4f61d625ff4b96574e548b284
AH Muzahid <ahmuzahid40@gmail.com>
2025-08-28 15:27:23 +0600

    Clear History Button Worked

---
+++ scipts/index.js
@@ -0,0 +1,7 @@
+// Call History Functionality
+const callHistoryList = document.getElementById('callHistoryList');
+
+document.getElementById("clearHistoryBtn").addEventListener("click", function() {
+    callHistoryList.innerHTML = "";
+    
+  });
