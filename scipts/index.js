3a2a41215880f439253c2d27041965ae47d478d0
AH Muzahid <ahmuzahid40@gmail.com>
2025-08-28 17:13:04 +0600

    heat count and history clear button working

--- scipts/index.js
+++ scipts/index.js
@@ -1,7 +1,26 @@
 // Call History Functionality
+
+
 const callHistoryList = document.getElementById('callHistoryList');
 
 document.getElementById("clearHistoryBtn").addEventListener("click", function() {
     callHistoryList.innerHTML = "";
-    
+
   });
+
+
+
+
+
+
+//   Heart Count Functionality
+let heartCount = 0;
+const heartCountDisplay = document.getElementById('heart-count');
+const heartButtons = document.querySelectorAll(".heart-btn");
+
+heartButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        heartCount++;
+        heartCountDisplay.textContent = heartCount;
+    });
+});
